635dbf4ac9972b2819eea6245831d58c8a833660
Ivarlottman <i.lottman@st.hanze.nl>
2025-10-07 09:00:01 +0200

    Updated Commandline parser to functioning level with 2 input files that calls group analyser method

--- MCT/src/main/java/nl/bioinf/Main.java
+++ MCT/src/main/java/nl/bioinf/Main.java
@@ -1,11 +1,13 @@
 package nl.bioinf;
 import nl.bioinf.io.CommandLineParser;
+import picocli.CommandLine;
 
 public class Main {
     public static void main(String[] args) {
         CommandLineParser parser = new CommandLineParser();
-        //System.out.println(parser);
-        parser.run();
+        int exitcode = new CommandLine(parser).execute(args);
+        System.out.println(exitcode+" exitcode");
+        System.exit(exitcode);
+
         }
     }
-// jar main
--- MCT/src/main/java/nl/bioinf/io/CommandLineParser.java
+++ MCT/src/main/java/nl/bioinf/io/CommandLineParser.java
@@ -1,5 +1,6 @@
 package nl.bioinf.io;
 
+import nl.bioinf.DataManagement.GroupAnalyser;
 import picocli.CommandLine.*;
 import javax.swing.*;
 import java.io.File;
@@ -10,6 +11,9 @@ import java.util.List;
 
 /**
  * Java documentation style class lvl
+ * located in /build/libs
+ * testcommand
+ * java -jar MTC-1.0-SNAPSHOT-all.jar /home/ivar-lottman/Desktop/bioinformatica/jaar_3/java_ap/Metagenomic_Comparison_Tool/Recources/config_1.txt /home/ivar-lottman/Desktop/bioinformatica/jaar_3/java_ap/Metagenomic_Comparison_Tool/Recources/config_2.txt
  *
  * @author Ivar
  * @version 0
@@ -17,30 +21,36 @@ import java.util.List;
 
 @Command(name = "CommandLineParser")
 public class CommandLineParser implements Runnable {
-    @Option(names = {"-o", "-output"}, description = "output file")
-    String outputFile;
+    @Option(names = {"-o", "-output"}, description = "output file, Default value: ${DEFAULT-VALUE}")
+    String outputFile = System.getProperty("user.dir");
 
-    @Option(names = {"-m", "method-type"}, description = "Method to be used in analysis: Mean or Median")
-    String methodType;
+    @Option(names = {"-m", "method-type"}, description = "Method to be used in analysis: Mean or Median." +
+            "Default value: ${DEFAULT-VALUE}")
+    StatsMethodType methodType = StatsMethodType.MEAN;
 
-    @Option(names = {"-a", "-analysis-type"}, description = "Type of analysis: number change or %")
-    String analysisType;
+    @Option(names = {"-a", "-analysis-type"}, description = "Type of analysis: number change or" +
+            " percentage change. Default value: ${DEFAULT-VALUE}")
+    AnalysisType analysisType = AnalysisType.NUMBER;
 
     @Option(names = {"-t", "-taxon"}, description =
             "Level of taxon (U)nclassified, (R)oot, (D)omain, (K)ingdom, " +
-                    "(P)hylum, (C)lass, (O)rder, (F)amily, (G)enus, (S)pecies or ALL.")
-    String taxonLevel;
+                    "(P)hylum, (C)lass, (O)rder, (F)amily, (G)enus, (S)pecies, (PC)Primitive Clasification " +
+                    "and (A)LL. " +
+                    "Default value: ${DEFAULT-VALUE}")
+    Taxon taxonLevel = Taxon.A;
 
-    @Option(names = {"-ct", "-countType"}, description = "Clade all or direct")
-    String countType;
+    @Option(names = {"-ct", "-countType"}, description = "Clade all or direct." +
+            " Default value: ${DEFAULT-VALUE}")
+     CountType countType = CountType.ALL;
 
-    @Option(names = {"-ac","-abundanceCount"}, description = "minimal abundance count")
-    int minAbundanceCount;
+    @Option(names = {"-ac","-abundanceCount"}, description = "minimal abundance count. " +
+            "Default value: ${DEFAULT-VALUE}")
+    int minAbundanceCount = 0;
 
     @Parameters(paramLabel = "Group 1", description = "First Config file for group 1")
     String configOne;
 
-    @Parameters(paramLabel = "Group 2", description = "Second config file for group 2")
+    @Parameters(paramLabel = "Group 2", description = "Second Config file for group 2")
     String configTwo;
 
     public Path getOutputFile(String outputFile) {
@@ -52,6 +62,18 @@ public class CommandLineParser implements Runnable {
 
     @Override
     public void run() {
+        System.out.println("Running command line parser");
+        //try catch
 
+        // TODO build testcases for min abundance count
+        Path configPathOne = Paths.get(configOne);
+        if (!Files.isReadable(configPathOne))
+        {throw new IllegalArgumentException("config file= "+configPathOne+ " is not readable");}
+        Path configPathTwo = Paths.get(configTwo);
+        if (!Files.isReadable(configPathTwo))
+        {throw new IllegalArgumentException("config file= "+configPathOne+ " is not readable");}
+
+        GroupAnalyser analyserObject = new GroupAnalyser(configPathOne, configPathTwo,
+                methodType, analysisType, taxonLevel, countType, minAbundanceCount);
     }
 }
