7521a60c3fe8f9d913522b15a0ca109a61bdaf54
Ivarlottman <i.lottman@st.hanze.nl>
2025-10-07 09:16:21 +0200

    Started Groupanalyser to pass commandline input into the Samplegroup object GroupAnalyser Added groupReader method to read config files SampleGroup object: modified create dataframe method to hashmap String, list<int> to NameAndGenus record, list<int> Modified constructor to use the readfile object and initilise private dataframe method

--- MCT/src/main/java/nl/bioinf/DataManagement/GroupAnalyser.java
+++ MCT/src/main/java/nl/bioinf/DataManagement/GroupAnalyser.java
@@ -1,4 +1,66 @@
 package nl.bioinf.DataManagement;
 
+import nl.bioinf.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class GroupAnalyser {
+    Path groupOnePath;
+    Path groupTwoPath;
+    SampleGroup sampleGroupOne;
+    SampleGroup sampleGroupTwo;
+    String groupOneName;
+    String groupTwoName;
+
+
+    public GroupAnalyser(Path groupOnePath, Path groupTwoPath, StatsMethodType statmethod,
+                         AnalysisType analysisType, Taxon taxonLevel,
+                         CountType countType, int minAbundanceCount) {
+        this.groupOnePath = groupOnePath;
+        this.groupOneName = groupOnePath.getFileName().toString();
+        this.groupTwoPath = groupTwoPath;
+        this.groupTwoName = groupTwoPath.getFileName().toString();
+        this.sampleGroupOne = new SampleGroup(groupReader(groupOnePath),
+                groupOneName, statmethod, countType, taxonLevel);
+        this.sampleGroupTwo = new SampleGroup(groupReader(groupTwoPath),
+                groupTwoName, statmethod, countType, taxonLevel);
+
+    }
+
+
+    private List<Path> groupReader(Path inputpath){
+        List<Path> paths = new ArrayList<>();
+        try(BufferedReader reader = Files.newBufferedReader(inputpath)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                //TODO add other checks here
+                if (line.isEmpty()) {continue;}
+
+                Path p = Paths.get(line);
+                if(Files.isReadable(p)){paths.add(p);}
+                else throw new IOException("File not readable "+line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return paths;
+    }
+
+    private void doStatistics(){}
+    private void printStatistics(){
+        nl.bioinf.io.FileWriter x = new FileWriter();
+    }
+    private void printTopTen(){
+        nl.bioinf.io.FileWriter x = new FileWriter();
+    }
+
+
+
+
 }
